1b3ad26d3c1021921074809ba5d7443bd5dbfbdc
iphyokafor <sandraifeoma22@gmail.com>
2020-06-27 07:42:51 +0100

    uodate: refactor project structure

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -66,18 +66,4 @@ export default {
       });
   },
 
-//   getAllUsers: async(req, res) => {
-//     try {
-//         const getUsers = await User.find({});
-//         return res.json({
-//             message: 'View all users',
-//             getUsers
-//         });
-
-//     } catch (err) {
-//         return res.json({
-//             message: err
-//         });
-//     }
-// },
 };
