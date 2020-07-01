5e7ace6647c66dd46fe78fab8464be2d669781e1
iphyokafor <sandraifeoma22@gmail.com>
2020-07-01 21:13:38 +0100

    implement dashboard redirect

--- client/app.js
+++ client/app.js
@@ -2,121 +2,137 @@
 //send it to backend
 //load dashboard
 
-const loginFormButton = document.querySelector('#login-button');
-if(loginFormButton){
-  loginFormButton.addEventListener('click', (e) => {
-e.preventDefault();
-const email = document.querySelector('#login-email').value;
-const password = document.querySelector('#login-password').value;
+const adminDashboard = document.querySelector('#admin-dashboard');
+const dashboard = document.querySelector('#dashboard');
+const userName = document.querySelector('#span-username');
+const adminUsername = document.querySelector('#admin-username');
 
-fetch('http://localhost:7000/api/auth/login', {
-  method: 'POST',
-  mode: 'cors',
-  body: JSON.stringify({
-    email, 
-    password
-  }),
-  headers: {
-    'Content-Type' : 'application/json'
-  }
-})
-.then(res =>{
-  console.log(res.status);
-  res.json()
+
+if(dashboard) {
+  userName.innerHTML = `${window.localStorage.name}`
 }
-)
-.then((data) => {
-  if (data.success === true) {
-    window.localStorage.token = data.data.token;
-    window.localStorage.admin = data.data.isAdmin;
-    window.localStorage.user = data.data.id;
-    document.querySelector('.message').style.display = 'block';
-    setTimeout(() => {
-      authLogin();
-    }, 8000);
+
+
+if (adminDashboard){
+  adminUsername.innerHTML = `${window.localStorage.name}`
+}
+
+const userAuth = () => {
+  if (window.localStorage.role === 'admin') {
+    window.location.replace('adminDashboard.html');
   } else {
-    // console.log("can't go");
-    let output = Object.keys(data).forEach((key) => {
-      output += `<p>${data[key]}<p/>`;
-    });
-    document.querySelector('.error').innerHTML = output;
-    document.querySelector('.error').style.display = 'block';
-    setTimeout(() => {
-      window.location.replace('login.html');
-    }, 5000);
+    window.location.replace('userDashboard.html');
   }
-})
-.catch((error) => {
-  // console.log(error);
-  document.querySelector('.error').innerHTML = '<h2>server error</h2>';
-  document.querySelector('.error').innerHTML = `<h3>${error}</h3>`;
-  setTimeout(() => {
-    window.location.replace('login.html');
-  }, 5000);
-});
-});
+};
+
+
+const loginFormButton = document.querySelector('#login-button');
+if (loginFormButton) {
+	loginFormButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		const email = document.querySelector('#login-email').value;
+		const password = document.querySelector('#login-password').value;
+
+		fetch('http://localhost:7000/api/auth/login', {
+			method: 'POST',
+			mode: 'cors',
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => 
+				res.json()
+			)
+			.then((data) => {
+        console.log(data);
+				if (data.status === 200) {
+					window.localStorage.token = data.token;
+          window.localStorage.role = data.user.role;
+          window.localStorage.name = `${data.user.firstName} ${data.user.lastName}`;
+					setTimeout(() => {
+						userAuth();
+					}, 2000);
+				} else {
+					// console.log("can't go");
+					let output = Object.keys(data).forEach((key) => {
+						output += `<p>${data[key]}<p/>`;
+					});
+					
+					setTimeout(() => {
+						window.location.replace('login.html');
+					}, 5000);
+				}
+			})
+			.catch((error) => {
+				// console.log(error);
+				document.querySelector('.error').innerHTML = '<h2>server error</h2>';
+				document.querySelector('.error').innerHTML = `<h3>${error}</h3>`;
+				setTimeout(() => {
+					window.location.replace('login.html');
+				}, 5000);
+			});
+	});
 }
- 
 
 const signupFormButton = document.querySelector('#signup-button');
 if (signupFormButton) {
-  signupFormButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const firstName = document.querySelector('#first-name').value;
-    console.log(firstName);
-    const lastName = document.querySelector('#last-name').value;
-    const email = document.querySelector('#signup-email').value;
-    const password = document.querySelector('#signup-password').value;
-    const companyName = document.querySelector('#company-name').value;
+	signupFormButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		const firstName = document.querySelector('#first-name').value;
+		console.log(firstName);
+		const lastName = document.querySelector('#last-name').value;
+		const email = document.querySelector('#signup-email').value;
+		const password = document.querySelector('#signup-password').value;
+		const companyName = document.querySelector('#company-name').value;
 
-    fetch('http://localhost:7000/api/auth/register', {
-      method: 'POST',
-      mode: 'cors',
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        password,
-        companyName
-        
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res =>{
-        console.log(res.status);
-        res.json()
-      }
-      )
-      .then((data) => {
+		fetch('http://localhost:7000/api/auth/register', {
+			method: 'POST',
+			mode: 'cors',
+			body: JSON.stringify({
+				firstName,
+				lastName,
+				email,
+				password,
+				companyName,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => {
+				console.log(res.status);
+				res.json();
+			})
+			.then((data) => {
         if (data.success === true) {
-          window.localStorage.token = data.data.token;
-          window.localStorage.admin = data.data.isAdmin;
-          window.localStorage.user = data.data.id;
-          document.querySelector('.message').style.display = 'block';
-          setTimeout(() => {
-            authSignup();
-          }, 8000);
-        } else {
-          // console.log("can't go");
-          let output = Object.keys(data).forEach((key) => {
-            output += `<p>${data[key]}<p/>`;
+					window.localStorage.token = data.token;
+          window.localStorage.role = data.user.role;
+          window.localStorage.name = `${data.user.firstName} ${data.user.lastName}`;
+					setTimeout(() => {
+						userAuth();
+					}, 2000);
+				} else {
+					// console.log("can't go");
+					let output = Object.keys(data).forEach((key) => {
+						output += `<p>${data[key]}<p/>`;
           });
-          document.querySelector('.error').innerHTML = output;
-          document.querySelector('.error').style.display = 'block';
-          setTimeout(() => {
-            window.location.replace('signup.html');
-          }, 5000);
-        }
-      })
-      .catch((error) => {
-        // console.log(error);
-        document.querySelector('.error').innerHTML = '<h2>server error</h2>';
-        document.querySelector('.error').innerHTML = `<h3>${error}</h3>`;
-        setTimeout(() => {
-          window.location.replace('signup.html');
-        }, 5000);
-      });
-  });
+          
+					setTimeout(() => {
+						window.location.replace('signup.html');
+					}, 5000);
+				}
+			})
+			.catch((error) => {
+				// console.log(error);
+				document.querySelector('.error').innerHTML = '<h2>server error</h2>';
+				document.querySelector('.error').innerHTML = `<h3>${error}</h3>`;
+				setTimeout(() => {
+					window.location.replace('signup.html');
+				}, 5000);
+			});
+	});
 }
